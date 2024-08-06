cb05d4cce5e8e8df760664503c510d5f4e169951
DevJohnnySantos <contatojohnnysantos@gmail.com>
2024-08-06 12:19:01 -0300

    String valueOf and charAt

---
+++ String/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) { String var1 = "juliano sobral";
+        double n1 = 13.4;
+
+        String converterParaString = String.valueOf(n1);
+        System.out.println(converterParaString.charAt(2));
+        System.out.println(converterParaString.charAt(2));
+        }
+    }
