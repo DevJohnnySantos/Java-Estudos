9bacadc350aaca71e056f3c4b4b004408262d3ee
DevJohnnySantos <contatojohnnysantos@gmail.com>
2024-08-06 12:41:00 -0300

    Update Strings

---
+++ tipos de Strings/trim/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String texto = "  no ceu tem pão  ";
+        String textoTrimmed = texto.trim();
+        System.out.println(textoTrimmed); // Output: "no ceu tem pão"
+
+    }
+}
